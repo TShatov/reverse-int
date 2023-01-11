560b78be04914474a638cb0a39ba567549b36a9a
TShatov <tshatov@gmail.com>
2023-01-11 13:30:49 +0300

    feat: reverse-int - resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let arr = String(n).split('');
+  let reversedArr = arr.reverse();
+
+  if (reversedArr.includes('-')) {
+    return reversedArr.slice(0, reversedArr.length-1).join('');
+  } else {
+    return reversedArr.join('');
+  }
 }
